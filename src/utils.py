45ae8965d5bd958f9891faded7e56455c6f1a50f
alih <alihassanijr1998@gmail.com>
2020-11-14 20:27:06 +0330

    Added norm storage for better performance

--- src/utils.py
+++ src/utils.py
@@ -14,11 +14,14 @@ def similarity_clamp(x):
     return torch.clamp(x, 0.0, 1.0)
 
 
-def distance_matrix(x, y):
-    x_norm = (x ** 2).sum(1).view(-1, 1)
-    y_t = torch.transpose(y, 0, 1)
-    y_norm = (y ** 2).sum(1).view(1, -1)
-    mat = x_norm + y_norm - 2.0 * torch.mm(x, y_t)
+def squared_norm(x):
+    return (x ** 2).sum(1).view(-1, 1)
+
+
+def distance_matrix(x, y, x_norm=None, y_norm=None):
+    x_norm = squared_norm(x) if x_norm is None else x_norm
+    y_norm = squared_norm(y) if y_norm is None else y_norm.T
+    mat = x_norm + y_norm - 2.0 * torch.mm(x, y.T)
     return distance_clamp(mat)
 
 
@@ -26,10 +29,14 @@ def self_distance_matrix(x):
     return distance_clamp(((x.unsqueeze(0) - x.unsqueeze(1)) ** 2).sum(2))
 
 
-def similarity_matrix(x, y):
+def similarity_matrix(x, y, pre_normalized=False):
+    if pre_normalized:
+        return similarity_clamp(x.matmul(y.T))
     return similarity_clamp(F.normalize(x, p=2, dim=1).matmul(F.normalize(y, p=2, dim=1).T))
 
 
-def self_similarity_matrix(x):
+def self_similarity_matrix(x, pre_normalized=False):
+    if pre_normalized:
+        return similarity_clamp(x.matmul(x.T))
     x_normalized = F.normalize(x, p=2, dim=1)
     return similarity_clamp(x_normalized.matmul(x_normalized.T))
