05e6154768ce3b2f8dfea78c384bc3c7e4d8d183
alih <alihassanijr1998@gmail.com>
2020-11-14 20:51:56 +0330

    Minor typesetting changes

--- src/_kmeanspp.py
+++ src/_kmeanspp.py
@@ -20,24 +20,27 @@ def k_means_pp(x, n_clusters, x_norm=None):
     x : torch.Tensor of shape (n_training_samples, n_features)
     n_clusters : int
     x_norm : torch.Tensor of shape (n_training_samples, )
+
+    Returns
+    -------
+    centroids : torch.Tensor of shape (n_clusters, n_features)
     """
     if x_norm is None:
         x_norm = squared_norm(x)
     n_samples, n_features = x.shape
 
-    centers = torch.zeros((n_clusters, n_features))
+    centroids = torch.zeros((n_clusters, n_features))
 
     n_local_trials = 2 + int(np.log(n_clusters))
 
     initial_centroid_idx = torch.randint(low=0, high=n_samples, size=(1,))[0]
-    centers[0, :] = x[initial_centroid_idx, :]
+    centroids[0, :] = x[initial_centroid_idx, :]
 
-    dist_mat = distance_matrix(x=centers[0, :].unsqueeze(0), y=x, y_norm=x_norm)
-    current_pot = dist_mat.sum(1)
+    dist_mat = distance_matrix(x=centroids[0, :].unsqueeze(0), y=x, y_norm=x_norm)
+    current_potential = dist_mat.sum(1)
 
-    # Pick the remaining n_clusters-1 points
     for c in range(1, n_clusters):
-        rand_vals = torch.rand(n_local_trials) * current_pot
+        rand_vals = torch.rand(n_local_trials) * current_potential
         candidate_ids = torch.searchsorted(torch.cumsum(dist_mat.squeeze(0), dim=0), rand_vals)
         torch.clamp_max(candidate_ids, dist_mat.size(1) - 1, out=candidate_ids)
 
@@ -45,13 +48,14 @@ def k_means_pp(x, n_clusters, x_norm=None):
 
         # TODO: torch implementation
         distance_to_candidates = torch.from_numpy(np.minimum(dist_mat.numpy(), distance_to_candidates.numpy()))
-        candidates_pot = distance_to_candidates.sum(1)
 
-        best_candidate = torch.argmin(candidates_pot)
-        current_pot = candidates_pot[best_candidate]
+        candidates_potential = distance_to_candidates.sum(1)
+
+        best_candidate = torch.argmin(candidates_potential)
+        current_potential = candidates_potential[best_candidate]
         dist_mat = distance_to_candidates[best_candidate].unsqueeze(0)
         best_candidate = candidate_ids[best_candidate]
 
-        centers[c, :] = x[best_candidate, :]
+        centroids[c, :] = x[best_candidate, :]
 
-    return centers
+    return centroids
