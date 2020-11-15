a785e79d9fe82fb4bc810db83d01934cc7b9f152
alih <alihassanijr1998@gmail.com>
2020-11-15 10:01:06 +0330

    Added DISCERN

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -7,3 +7,4 @@ Common util functions
 
 from .metrics import distance_matrix, self_distance_matrix, similarity_matrix, self_similarity_matrix
 from .norms import row_norm, squared_norm
+from .tools import torch_unravel_index
